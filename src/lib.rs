f487ecdc88425beb52ec778bdfb80831e66afd42
shengj.sang <shengj.sang@icloud.com>
2023-01-08 13:00:01 +0800

    feat: fn -> generate

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 
 pub struct Custom {
     /// length of random number or string
-    length: i32,
+    length: usize,
     charset: CharSet,
 }
 
 
-pub struct  CharSet {
+pub struct CharSet {
     kind: CharSetKind,
     value: Vec<u8>,
 }
@@ -23,10 +23,46 @@ pub enum CharSetKind {
 }
 
 impl Custom {
-    pub fn new(length: i32, charset: CharSet) -> Custom {
+    pub fn new(length: usize, charset: CharSet) -> Custom {
         Custom {
             length,
             charset,
         }
+    }
+
+    pub fn generate(&self) -> Vec<u8> {
+        let mut charset: Vec<u8> = Vec::new();
+        match self.charset.kind {
+            CharSetKind::Number => {
+                charset = b"0123456789".to_vec();
+            }
+            CharSetKind::Letter => {
+                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            ".to_vec();
+            }
+            CharSetKind::Symbol => {
+                charset = b"~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+            }
+            CharSetKind::NumberAndLetter => {
+                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789".to_vec();
+            }
+            CharSetKind::NumberAndSymbol => {
+                charset = b"0123456789~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+            }
+            CharSetKind::LetterAndSymbol => {
+                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                           ~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+            }
+            CharSetKind::NumberLetterAndSymbol => {
+                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+            }
+        }
+        charset
     }
 }
