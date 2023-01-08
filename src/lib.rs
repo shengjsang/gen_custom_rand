90af33ed1302dd173a6e71a1ce68a8869534f559
shengj.sang <shengj.sang@icloud.com>
2023-01-08 10:59:45 +0800

    feat: struct CharSet

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+/// Generate random number or string by custom
+
+pub struct Custom {
+    /// length of random number or string
+    length: i32,
+    charset: CharSet,
+}
+
+
+pub struct  CharSet {
+    kind: CharSetKind,
+    value: Vec<u8>,
+}
+
+pub enum CharSetKind {
+    Number,
+    Letter,
+    Symbol,
+    NumberAndLetter,
+    NumberAndSymbol,
+    LetterAndSymbol,
+    NumberLetterAndSymbol,
+}
