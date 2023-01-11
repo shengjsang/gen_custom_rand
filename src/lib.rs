fcde2ae0a55e14ff616ef5c4ed35fdf496569278
shengj.sang <shengj.sang@icloud.com>
2023-01-11 12:10:44 +0800

    feat: tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,13 @@
 /// Generate random number or string by custom
 
-use anyhow::Result;
 use rand::Rng;
 
 pub struct Custom {
     /// length of random number or string
     length: usize,
-    charset: CharSet,
-}
-
-
-pub struct CharSet {
     kind: CharSetKind,
-    value: Vec<u8>,
 }
+
 
 pub enum CharSetKind {
     Number,
@@ -26,59 +20,68 @@ pub enum CharSetKind {
 }
 
 impl Custom {
-    pub fn new(length: usize, charset: CharSet) -> Custom {
+    pub fn new(length: usize, kind: CharSetKind) -> Custom {
         Custom {
             length,
-            charset,
+            kind,
         }
     }
 
     pub fn generate(&self) -> String {
-        let mut charset: Vec<u8> = Vec::new();
-        match self.charset.kind {
+
+        let charset = match self.kind {
             CharSetKind::Number => {
-                charset = b"0123456789".to_vec();
+                b"0123456789".to_vec()
             }
             CharSetKind::Letter => {
-                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
-                            ".to_vec();
+                            ".to_vec()
             }
             CharSetKind::Symbol => {
-                charset = b"~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+                b"~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec()
             }
             CharSetKind::NumberAndLetter => {
-                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
-                            0123456789".to_vec();
+                            0123456789".to_vec()
             }
             CharSetKind::NumberAndSymbol => {
-                charset = b"0123456789~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+                b"0123456789~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec()
             }
             CharSetKind::LetterAndSymbol => {
-                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
-                           ~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+                           ~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec()
             }
             CharSetKind::NumberLetterAndSymbol => {
-                charset = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                             abcdefghijklmnopqrstuvwxyz\
-                            0123456789~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec();
+                            0123456789~!@#$%^&*()_+-={}[]|:;<>,.?/\"\\".to_vec()
             }
-        }
+        };
 
         let mut rng = rand::thread_rng();
 
         let value: String = (0..self.length)
             .map(|_| {
                 let idx = rng.gen_range(0..charset.len());
-                CHARSET[idx] as char
+                charset[idx] as char
             })
             .collect();
 
         value
-
     }
+}
 
 
+#[cfg(test)]
+mod tests {
+    use crate::{CharSetKind, Custom};
+
+    #[test]
+    fn it_works() {
+        let random = Custom::new(5, CharSetKind::NumberAndLetter).generate();
+        assert_eq!(random, "a".to_string())
+    }
 }
